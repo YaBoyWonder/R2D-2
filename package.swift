229f7aa2182eec539a1a59ae95e1d7341344b049
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-13 18:36:43 -0400

    Create package.swift

---
+++ package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "R2D-2 Discord Bot",
+    dependencies: [
+        .Package(url: "https://github.com/Azoy/Sword", majorVersion: 0, minor: 4)
+    ]
+)
