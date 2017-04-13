1edcb57b5c6eae5358a024a4d5f288175548903d
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-13 19:13:10 -0400

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,8 +9,20 @@ bot.register("ping", message: "Pong!")
 
 bot.register("owner", message: "Wonder™#3350")
 
-bot.register("echo") { msg, args in
-  msg.reply(with: args.joined(separator: " "))
+bot.register("join") { message, args in
+  guard message.member?.voiceState != nil else {
+    message.reply(with: "Voice Channel not found")
+
+    return
+  }
+
+  bot.join(voiceChannel: message.member!.voiceState!.channelId) { connection in
+    connection.play(Youtube("https://www.youtube.com/watch?v=5lNyCSeO3BM"))
+  }
+}
+
+bot.register("echo") { message, args in
+  message.reply(with: args.joined(separator: " "))
 }
 
 bot.connect()
