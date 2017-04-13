0583906f3e6f258b290dad036d6fd8ed64a55dea
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-13 19:23:48 -0400

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,9 +5,19 @@ options.prefixes = ["r."]
 
 let bot = Shield(token: "Bot", shieldOptions: options)
 
-bot.register("ping", message: "Pong!")
+bot.on(.messageCreate) { data in
+  let msg = data[0] as! Message
+  if msg.content == "r.ping" {
+   let embed = ["embed": [
+  "title": "Owner",
+  "description": "Wonder™#3350"
+]]
 
-bot.register("owner", message: "Wonder™#3350")
+bot.send(embed, to: channelId)
+  }
+}
+
+bot.register("ping", message: "Pong!")
 
 bot.register("join") { message, args in
   guard message.member?.voiceState != nil else {
@@ -19,6 +29,16 @@ bot.register("join") { message, args in
   bot.join(voiceChannel: message.member!.voiceState!.channelId) { connection in
     connection.play(Youtube("https://www.youtube.com/watch?v=5lNyCSeO3BM"))
   }
+}
+
+bot.register("leave") { msg, args in
+  guard msg.member?.voiceState != nil else {
+    msg.reply(with: "User not located.")
+
+    return
+  }
+
+  bot.leave(voiceChannel: msg.member!.voiceState!.channelId)
 }
 
 bot.register("echo") { message, args in
